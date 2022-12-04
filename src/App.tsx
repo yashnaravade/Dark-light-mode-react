e2b5294694ea1db2192d787f4db3f550bdf617bd
Yash Naravade <yashdip123@gmail.com>
2022-12-05 01:09:30 +0530

    Add toggle switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Form from "./components/Form";
+import ReactSwitch from "react-switch";
 
 export const ThemeContext = React.createContext("null");
 
 function App() {
-  const [theme, setTheme] = React.useState("light");
+  const [theme, setTheme] = React.useState("dark");
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -15,6 +16,10 @@ function App() {
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <Form />
+        <div className="switch">
+          <label> {theme === "dark" ? "Dark Mode" : "Light Mode"} </label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
       </div>
     </ThemeContext.Provider>
   );
