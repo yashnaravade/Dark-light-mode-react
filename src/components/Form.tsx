2c0a21db70f2473b971bf7326cb218c12bb4c457
Yash Naravade <yashdip123@gmail.com>
2022-12-04 19:08:57 +0530

    first commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Form.css";
+function Form() {
+  return (
+    <>
+   <div className="main">
+    <p className="sign" align="center">Sign in</p>
+    <form className="form1">
+      <input className="un " type="text" align="center" placeholder="Username"/>
+      <input className="pass" type="password" align="center" placeholder="Password"/>
+      <a className="submit" align="center">Sign in</a>
+      <p className="forgot" align="center"><a href="#">Forgot Password?</a></p>
+    </form>   
+    </div>
+
+    </>
+  );
+}
+
+export default Form;
